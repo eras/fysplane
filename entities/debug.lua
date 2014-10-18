587b9875776a1933fc949a1f7ba2adb4d3b82bde
Erkki Seppälä <flux@modeemi.fi>
2014-10-18 22:23:04 +0300

    debug: added labesl to debugging

--- entities/debug.lua
+++ entities/debug.lua
@@ -1,17 +1,32 @@
 class = require 'hump.class'
+VectorLight = require 'hump/vector-light'
+
+local scale = 10
+
+local font = love.graphics.newFont(10)
 
 DebugVector = Class {
-    init = function(self, x0 , y0, dx, dy)
+    init = function(self, label, x0 , y0, dx, dy)
         self.x0 = x0;
         self.y0 = y0;
         self.dx = dx;
         self.dy = dy;
+        self.label = label;
     end;
 
 
     draw = function(self)
         love.graphics.setColor(255, 0, 0)
-        love.graphics.line(self.x0, self.y0, self.x0 + self.dx, self.y0 + self.dy);
+        love.graphics.line(self.x0, self.y0, self.x0 + self.dx * scale, self.y0 + self.dy * scale);
+        if self.label then
+            love.graphics.setColor(0,0,0)
+            love.graphics.setFont(font)
+            local unit_x, unit_y = VectorLight.div(VectorLight.len(self.dx, self.dy), self.dx, self.dy)
+            local dist_x, dist_y = self.dx * scale, self.dy * scale
+            local distance = math.min(100, VectorLight.len(dist_x, dist_y))
+            dist_x, dist_y = VectorLight.mul(distance, unit_x, unit_y)
+            love.graphics.printf(self.label, self.x0 + dist_x, self.y0 + dist_y, 100, "left")
+        end
     end;
 }
 
