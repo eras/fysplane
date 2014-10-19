8a009ddbfd630823fd1eaec58d90fbaf4d94653e
Erkki Seppälä <flux@modeemi.fi>
2014-10-19 07:11:15 +0300

    Debug: support for debug circles

--- entities/debug.lua
+++ entities/debug.lua
@@ -25,15 +25,38 @@ DebugVector = Class {
             local dist_x, dist_y = self.dx * scale, self.dy * scale
             local distance = math.min(100, VectorLight.len(dist_x, dist_y))
             dist_x, dist_y = VectorLight.mul(distance, unit_x, unit_y)
-            love.graphics.printf(self.label, self.x0 + dist_x, self.y0 + dist_y, 100, "left")
+            love.graphics.printf(string.format("%s %f", self.label, VectorLight.len(self.dx, self.dy)), self.x0 + dist_x, self.y0 + dist_y, 100, "left")
         end
+    end;
+}
+
+DebugCircle = Class {
+    init = function(self, label, x0 , y0)
+        self.x0 = x0;
+        self.y0 = y0;
+        self.label = label;
+    end;
+
+
+    draw = function(self)
+        love.graphics.setColor(255, 0, 0)
+        love.graphics.circle("fill", self.x0, self.y0, 5, 20);
+        -- if self.label then
+        --     love.graphics.setColor(0,0,0)
+        --     love.graphics.setFont(font)
+        --     local unit_x, unit_y = VectorLight.div(VectorLight.len(self.dx, self.dy), self.dx, self.dy)
+        --     local dist_x, dist_y = self.dx * scale, self.dy * scale
+        --     local distance = math.min(100, VectorLight.len(dist_x, dist_y))
+        --     dist_x, dist_y = VectorLight.mul(distance, unit_x, unit_y)
+        --     love.graphics.printf(string.format("%s %f", self.label, VectorLight.len(self.dx, self.dy)), self.x0 + dist_x, self.y0 + dist_y, 100, "left")
+        -- end
     end;
 }
 
 debugEnabled = false
 
 drawDebug = function(debugVectors) 
-    if debugEnabled then
+    if debugEnabled and debugVectors then
         for key, debugVector in pairs(debugVectors) do
             debugVector:draw();
         end
