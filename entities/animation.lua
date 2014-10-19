ffa39098879c1a8a52209e77a723c3f79a109fa0
Erkki Seppälä <flux@modeemi.fi>
2014-10-19 03:26:23 +0300

    Animation: fixed

--- entities/animation.lua
+++ entities/animation.lua
@@ -6,31 +6,32 @@ Animation = Class{
 
     frames = {},
 
-    curFrame = 0
+    curFrame = 0,
 
     init = function(self, x, y, level, basename, numFrames, fps)
         Entity.init(self, x, y, level)
 
-        for frame = 0,numFrames - 1 do
+        for frame = 0, numFrames - 1 do
             self.frames[frame] = love.graphics.newImage(string.format(basename, frame))
         end
 
+        self.fps = fps
         self.angle = 0
-        self.width = self.frames[0].getWidth();
-        self.height = self.frames[0].getHeight();
+        self.x = x
+        self.y = y
+        self.width = self.frames[0]:getWidth();
+        self.height = self.frames[0]:getHeight();
         self.quad = love.graphics.newQuad(0, 0, self.width, self.height, self.width, self.height)
     end;
 
     draw = function(self)
-        if self.health > 0 then
-            local img = self.frames[math.floor(curFrame)]
-            if img then
-                love.graphics.draw(self.frames[0], self.quad, 0, 0, self.angle, 1, 1, self.width / 2, self.width / 2)
-            end
+        local img = self.frames[math.floor(self.curFrame)]
+        if img then
+            love.graphics.draw(img, self.quad, self.x, self.y, self.angle, 1, 1, self.width / 2, self.width / 2)
         end
     end;
 
     update = function(self, dt)
-        curFrame += dt * fps
+        self.curFrame = self.curFrame + dt * self.fps
     end;
 }
